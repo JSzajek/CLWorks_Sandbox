2cb3fe8b41b88012b5dc9d128ea6a5990cfcbb47
jszajek <jszajek2@gmail.com>
2025-05-05 21:58:48 -0500

    Initial Commit.

---
+++ Source/CLWorks_Sandbox/CLWorks_Sandbox.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CLWorks_Sandbox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CLWorks_Sandbox, "CLWorks_Sandbox" );
